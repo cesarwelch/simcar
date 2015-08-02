770c9cec6e04189b8692998587c6e339be3088ca
Cesar Welch <cesar777welch@gmail.com>
2015-08-01 21:41:32 -0600

    Car
    added car class and declaracion de carro en form

--- simcar/simcar/Form1.Designer.cs
+++ simcar/simcar/Form1.Designer.cs
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Velocidad";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(597, 273);
-            this.Name = "Simulador";
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Simulador de carro";
             this.ResumeLayout(false);
-            int vel = 0;
+            this.PerformLayout();
+            car car1 = new car(1, 2, 3, 4, 5);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ simcar/simcar/car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simcar
+{
+    public class car
+    {
+        
+        
+        public int tipo { get; set; } //tipo de carro 1 liviano 2 camioneta 3 pesada
+        public int peso { get; set; } //peso del carro
+        public int velmax { get; set; } //velocidad maxima del carro
+        public int maxacel { get; set; } //maxima aceleracion del carro
+        public int actgas { get; set; } // gasolina actual
+        public int velact { get; set; } // velocidad actual
+        public int acelact { get; set; }// acelelarcion actual actual
+        public car(int tipo, int peso, int velmax, int maxacel, int actgas) { }
+
+
+    }
+
+
+}
